ccc9d4c21862c1bac37bf9a56d6b83da73891513
Venil Noronha <veniln@vmware.com>
2019-02-21 11:31:10 -0800

    Add Go server and client for the emoji service
    go run -v cmd/server.go
    go run -v cmd/client.go --server 'localhost:9000' --text 'I like :pizza: and :sushi:!'
    
    Signed-off-by: Venil Noronha <veniln@vmware.com>

---
+++ cmd/client.go
@@ -0,0 +1,43 @@
+// Copyright 2019 VMware, Inc.
+// SPDX-License-Identifier: BSD-3-Clause
+
+package main
+
+import (
+	"flag"
+	"log"
+	"time"
+
+	"github.com/venilnoronha/grpc-web-demo/proto"
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+)
+
+var server = flag.String("server", "localhost:9000", "The server's address")
+var text = flag.String("text", "Hello world!", "The input text")
+
+func init() {
+	flag.Parse()
+}
+
+func main() {
+	conn, err := grpc.Dial(*server, grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("Couldn't connect to the service: %v", err)
+	}
+	defer conn.Close()
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	c := proto.NewEmojiServiceClient(conn)
+
+	log.Printf("Request: %s", *text)
+	res, err := c.InsertEmojis(ctx, &proto.EmojiRequest{
+		InputText: *text,
+	})
+	if err != nil {
+		log.Fatalf("Couldn't call service: %v", err)
+	}
+	log.Printf("Server says: %s", res.OutputText)
+}
---
+++ cmd/server.go
@@ -0,0 +1,39 @@
+// Copyright 2019 VMware, Inc.
+// SPDX-License-Identifier: BSD-3-Clause
+
+package main
+
+import (
+	"context"
+	"log"
+	"net"
+
+	"github.com/venilnoronha/grpc-web-demo/proto"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+	emoji "gopkg.in/kyokomi/emoji.v1"
+)
+
+type server struct{}
+
+func (s *server) InsertEmojis(ctx context.Context, req *proto.EmojiRequest) (*proto.EmojiResponse, error) {
+	log.Printf("Client says: %s", req.InputText)
+	outputText := emoji.Sprint(req.InputText)
+	log.Printf("Response: %s", outputText)
+	return &proto.EmojiResponse{OutputText: outputText}, nil
+}
+
+func main() {
+	lis, err := net.Listen("tcp", ":9000")
+	if err != nil {
+		log.Fatalf("Failed to listen: %v", err)
+	}
+	log.Printf("Listening on %s", lis.Addr())
+
+	s := grpc.NewServer()
+	proto.RegisterEmojiServiceServer(s, &server{})
+	reflection.Register(s)
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("Failed to serve: %v", err)
+	}
+}
